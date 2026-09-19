63a9e588633466c6a8367969db0545d29be92fb8
getSandi <mresandis@gmail.com>
2026-09-20 06:14:05 +0700

    Initia Cypress automation project

---
+++ cypress/e2e/sanbercode-automation/sanbercode-automation.cy.js
@@ -0,0 +1,206 @@
+describe('OrangeHRM - Login Feature', () => {
+
+    beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    });
+
+    // TS-LOGIN-001
+    it('TC-LOGIN-001 - Login dengan kredensial valid', () => {
+
+        cy.get('input[name="username"]')
+            .type('Admin');
+
+        cy.get('input[name="password"]')
+            .type('admin123');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.url()
+            .should('include', '/dashboard');
+
+        cy.contains('Dashboard')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-002
+    it('TC-LOGIN-002 - Login dengan username tidak valid', () => {
+
+        cy.get('input[name="username"]')
+            .type('MasSandi');
+
+        cy.get('input[name="password"]')
+            .type('admin123');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.contains('Invalid credentials')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-003
+    it('TC-LOGIN-003 - Login dengan password tidak valid', () => {
+
+        cy.get('input[name="username"]')
+            .type('Admin');
+
+        cy.get('input[name="password"]')
+            .type('MasSandi');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.contains('Invalid credentials')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-004
+    it('TC-LOGIN-004 - Login dengan username dan password tidak valid', () => {
+
+        cy.get('input[name="username"]')
+            .type('MasSandi');
+
+        cy.get('input[name="password"]')
+            .type('MasSandi');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.contains('Invalid credentials')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-005
+    it('TC-LOGIN-005 - Login dengan username kosong', () => {
+
+        cy.get('input[name="password"]')
+            .type('admin123');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.get('input[name="username"]')
+            .parents('.oxd-input-group')
+            .contains('Required')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-006
+    it('TC-LOGIN-006 - Login dengan password kosong', () => {
+
+        cy.get('input[name="username"]')
+            .type('Admin');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.get('input[name="password"]')
+            .parents('.oxd-input-group')
+            .contains('Required')
+            .should('be.visible');
+    });
+
+
+   
+    // TS-LOGIN-007
+    it('TC-LOGIN-007 - Login dengan kedua field kosong', () => {
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.get('.oxd-input-group__message')
+            .should('have.length', 2)
+            .each(($error) => {
+                expect($error.text()).to.equal('Required');
+            });
+
+    });
+
+
+
+    // TS-LOGIN-008
+    it('TC-LOGIN-008 - Password ditampilkan sebagai karakter tersembunyi', () => {
+
+        cy.get('input[name="password"]')
+            .type('admin123');
+
+        cy.get('input[name="password"]')
+            .should('have.attr', 'type', 'password');
+    });
+
+
+    // TS-LOGIN-009
+    it('TC-LOGIN-009 - Username bersifat case-sensitive', () => {
+
+        cy.get('input[name="username"]')
+            .type('admin');
+
+        cy.get('input[name="password"]')
+            .type('admin123');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.contains('Invalid credentials')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-010
+    it('TC-LOGIN-010 - Password bersifat case-sensitive', () => {
+
+        cy.get('input[name="username"]')
+            .type('Admin');
+
+        cy.get('input[name="password"]')
+            .type('ADMIN123');
+
+        cy.get('button[type="submit"]')
+            .click();
+
+        cy.contains('Invalid credentials')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-011
+    it('TC-LOGIN-011 - Akses Forgot your password', () => {
+
+        cy.contains('Forgot your password?')
+            .click();
+
+        cy.url()
+            .should('include', '/requestPasswordResetCode');
+
+        cy.get('input[name="username"]')
+            .should('be.visible');
+
+        cy.contains('Reset Password')
+            .should('be.visible');
+    });
+
+
+    // TS-LOGIN-012
+    it('TC-LOGIN-012 - Login menggunakan tombol Enter', () => {
+
+        cy.get('input[name="username"]')
+            .type('Admin');
+
+        cy.get('input[name="password"]')
+            .type('admin123')
+            .type('{enter}');
+
+        cy.url()
+            .should('include', '/dashboard');
+
+        cy.contains('Dashboard')
+            .should('be.visible');
+    });
+
+});
